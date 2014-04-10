312256f07c95248264eb86451ef2c9970f4fa67e
James Moger <james.moger@gitblit.com>
2014-04-10 16:02:54 -0400

    Added powertools 1.1.0

---
+++ checksums.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Calculates the SHA1 and MD5 hashes for the registry.
+#
+
+function calc() {
+  local file=$1
+  sha1sum $file | cut -d" " -f1 > "$file".sha1
+  md5sum $file | cut -d" " -f1 > "$file".md5
+}
+
+calc plugins.json
+
